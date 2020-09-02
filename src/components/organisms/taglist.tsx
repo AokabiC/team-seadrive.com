7f9af58efbd5e3131eb3ba87de45c0e15912ca26
AokabiC <syht0312@gmail.com>
2020-09-03 07:53:15 +0900

    add tag toggle

--- src/components/organisms/taglist.tsx
+++ src/components/organisms/taglist.tsx
@@ -1,19 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
 import { graphql, useStaticQuery } from "gatsby"
 import Anchor from "../atoms/anchor"
+import color from "@/utils/color"
+
+const Tag = styled(Anchor.In)<{ toggle: boolean }>`
+  border-radius: 0.2rem;
+  padding: 0 0.2rem;
+  background: ${props =>
+    props.toggle ? "rgba(0,0,200,0.2)" : "rgba(0,0,0,0)"};
+  color: ${color.primary_dark};
+`
 
 const TagList: React.FC<any> = () => {
   const data = useStaticQuery(TagQuery)
   const tags = data.allMarkdownRemark.group
+  const [toggle, setToggle] = useState(new Map<string, boolean>())
+
   return (
     <Container>
       {tags.map((tag: any) => (
         <li key={tag.fieldValue}>
-          <Anchor.In to={`/tags/${tag.fieldValue}/`}>
+          <Tag
+            toggle={
+              toggle.get(tag.fieldValue) ? toggle.get(tag.fieldValue)! : false
+            }
+            onClick={() => {
+              setToggle(toggle.set(tag.fieldValue, !toggle.get(tag.fieldValue)))
+            }}
+            to={"/blog/"}
+          >
             {`#${tag.fieldValue}`}
-          </Anchor.In>
+          </Tag>
         </li>
       ))}
     </Container>
@@ -26,7 +45,7 @@ const Container = styled.ul`
   flex-wrap: wrap;
   margin-bottom: 1rem;
   li {
-    margin-right: 1rem;
+    margin-right: 0.5rem;
   }
 `
 

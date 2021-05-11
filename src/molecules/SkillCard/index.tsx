d1a397d103ee10d3807bf165a96d86aec6dd543b
T.Hosoya <syht0312@gmail.com>
2021-05-11 12:11:43 +0900

    feat: add skill page (#52)
    * add percentage bar
    
    * create skill card
    
    * feat: add skill page

---
+++ src/molecules/SkillCard/index.tsx
@@ -0,0 +1,94 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { PercentageBar } from "atoms/PercentageBar";
+import { Tag } from "atoms/Tag";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { useIntersection } from "react-use";
+import * as Styled from "./styled";
+
+interface Props {
+  name: string;
+  src?: string;
+  tags?: Array<string>;
+  value?: number;
+}
+
+export const SkillCard: React.FC<Props> = ({
+  name,
+  src,
+  value,
+  tags,
+  children,
+}) => {
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.02,
+  });
+  const [intersected, setIntersected] = useState(false);
+  useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setIntersected(true);
+    }
+  }, [intersection]);
+  return (
+    <Styled.Container
+      ref={intersectionRef}
+      css={css`
+        opacity: ${intersected ? 1 : 0};
+        transform: ${intersected ? "translateY(0)" : "translateY(2rem)"};
+      `}
+    >
+      <div
+        css={(theme) => css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin: ${theme.space.xs} 0;
+        `}
+      >
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          {src && <Image src={src} alt={name} height="32px" width="32px" />}
+          <Styled.Title>{name}</Styled.Title>
+        </div>
+        <div
+          css={css`
+            display: flex;
+            flex-wrap: wrap;
+          `}
+        >
+          {tags &&
+            tags.map((tag) => (
+              <Tag key={tag} href={"/"}>
+                {tag}
+              </Tag>
+            ))}
+        </div>
+      </div>
+      {value && (
+        <>
+          <div
+            css={(theme) => css`
+              display: flex;
+              justify-content: space-between;
+              font-size: ${theme.fontSize.small};
+              margin-bottom: 2px;
+            `}
+          >
+            <span>Novice</span>
+            <span>Advanced</span>
+          </div>
+          <PercentageBar value={value} />
+        </>
+      )}
+      <p>{children}</p>
+    </Styled.Container>
+  );
+};
---
+++ src/molecules/SkillCard/styled.tsx
@@ -0,0 +1,22 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const Container = styled.div(
+  ({ theme }) => css`
+    background-color: ${theme.color.surfacePrimary};
+    padding: ${theme.space.sm} ${theme.space.md};
+    margin-bottom: ${theme.space.md};
+    border-radius: ${theme.borderRadius.sm};
+    transition: opacity ${theme.transitionSpeed.hover} ease-in-out,
+      transform ${theme.transitionSpeed.hover} ease-in-out;
+  `
+);
+
+export const Title = styled.h2(
+  ({ theme }) => css`
+    margin: ${theme.space.sm} ${theme.space.xs};
+    color: ${theme.color.textAccent};
+    font-size: ${theme.fontSize.heading};
+    font-weight: ${theme.fontWeight.heading};
+  `
+);

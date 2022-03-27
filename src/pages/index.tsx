56e1bc2d64f3a0067a939a190cf6728b1edea764
AokabiC <syht0312@gmail.com>
2022-03-28 03:35:44 +0900

    delete intersection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
 import { NextPage } from "next";
 import { Hero } from "organisms/Hero";
 import React from "react";
-import { useIntersection } from "react-use";
 import { CommonLayout } from "templates/Common";
 import { SEO } from "utils/Seo";
 
 const IndexPage: NextPage = () => {
-  const intersectionRef = React.useRef(null);
-  const intersection = useIntersection(intersectionRef, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.02,
-  });
   return (
-    <CommonLayout inverse={!intersection?.isIntersecting ?? false}>
+    <CommonLayout inverse={true}>
       <SEO />
-      <Hero isVisible={!intersection?.isIntersecting ?? false} />
+      <Hero isVisible={true} />
     </CommonLayout>
   );
 };
